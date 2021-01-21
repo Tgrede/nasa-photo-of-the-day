4a44feccde5f420373bb2d970480f73a55fd5ce2
Trenten grede <Trenten.grede@gmail.com>
2021-01-20 23:17:46 -0600

    ran build command

--- src/HeaderBar.js
+++ src/HeaderBar.js
@@ -1,8 +1,7 @@
-import React, {useState} from 'react'
+import React from 'react'
 
 
 export default function HeaderBar(props) {
-     const [date, setDate] = useState('')
      const getDate = () => {
           const today = new Date()
           return today.toLocaleDateString()
