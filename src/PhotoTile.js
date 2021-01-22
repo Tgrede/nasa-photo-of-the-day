fce279d80b5aa31b2282d28a0bbaa42d9383a70f
Trenten grede <Trenten.grede@gmail.com>
2021-01-21 23:32:53 -0600

    reupdated style

--- src/PhotoTile.js
+++ src/PhotoTile.js
@@ -12,7 +12,7 @@ export default function PhotoTile(props) {
 }
 
 const StyledPhotoTile = styled.img`
-     width: 100%;
+     width: 90%;
      border-radius: 5%;
      box-shadow: 5px 5px 3px black
 `
